6bbfe88122255eed0bd9eada110b17d37f81240f
gage-fend <75444711+gage-fend@users.noreply.github.com>
2020-12-08 10:06:45 -0800

    Update Window.java

--- BattleShip/src/battleship/Window.java
+++ BattleShip/src/battleship/Window.java
@@ -1,15 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package battleship;
 
-
-/**
- *
- * @author jeffreyyee
- */
 public class Window {
     private static final int XBORDER = 20;
 //    private static final int YBORDER = 20;
