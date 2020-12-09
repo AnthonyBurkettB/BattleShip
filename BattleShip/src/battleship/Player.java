0aa0e70eb4adfbb0b770165288be236de016a771
diegod17 <75444675+diegod17@users.noreply.github.com>
2020-12-09 09:57:47 -0800

    Update Player.java

--- BattleShip/src/battleship/Player.java
+++ BattleShip/src/battleship/Player.java
@@ -1,1 +1,40 @@
+package battleship;
+import java.awt.Color;
 
+public class Player {
+    
+    private static Player currentTurn;
+    private static Player players[] = new Player[2];
+  
+    public static void Reset()
+    {
+        if (players[0] == null) {
+            
+        }
+        currentTurn = players[0];
+    }
+   
+    public static Player GetCurrentPlayer()
+    {
+        return(currentTurn);
+    }
+
+    public static void SwitchTurn()
+    {
+        if (currentTurn == players[0])
+            currentTurn = players[1];
+        else
+            currentTurn = players[0];
+    }    
+    
+    Player(Color _color)
+    {
+        color = _color;
+    }
+    public Color getColor()
+    {
+        return (color);
+    }
+
+        
+}
