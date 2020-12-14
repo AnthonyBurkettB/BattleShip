420dcc37c50be9a3c8e057bfa65dc1c217e4b08f
AnthonyBurkettB <75444915+AnthonyBurkettB@users.noreply.github.com>
2020-12-14 11:10:58 -0800

    Update Player.java

--- BattleShip/src/battleship/Player.java
+++ BattleShip/src/battleship/Player.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package battleship;
 import java.awt.Color;
 
@@ -27,14 +32,14 @@ public class Player {
             currentTurn = players[0];
     }    
     
-    Player(Color _color)
+   /* Player(Color _color)
     {
         color = _color;
     }
     public Color getColor()
     {
         return (color);
-    }
+    }  */
 
         
 }
