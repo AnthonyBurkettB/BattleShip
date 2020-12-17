e350f3073eda58fd4d8195764b4766ecd0f8b439
AnthonyBurkettB <75444915+AnthonyBurkettB@users.noreply.github.com>
2020-12-17 10:28:15 -0800

    Update Board.java

--- BattleShip/src/battleship/Board.java
+++ BattleShip/src/battleship/Board.java
@@ -14,9 +14,15 @@ public class Board {
     private final static int NUM_ROWS = 8;
     private final static int NUM_COLUMNS = 8;
     private final static int CordNUM_ROWS= 8;
-    private final static int CordNUM_COLUMNS= 8;      
+    private final static int CordNUM_COLUMNS= 8;  
+    private final static int P2NUM_ROWS= 8;  
+    private final static int P2NUM_COLUMNS= 8;
+    private final static int P2CordNUM_ROWS= 8;
+    private final static int P2CordNUM_COLUMNS= 8;
     private static Ship board[][] = new Ship[NUM_ROWS][NUM_COLUMNS];
     private static Ship MBoard[][] = new Ship[CordNUM_ROWS][CordNUM_COLUMNS];
+    private static Ship P2Board[][] = new Ship[P2NUM_ROWS][P2NUM_COLUMNS];
+    private static Ship P2MBoard[][] = new Ship[P2CordNUM_ROWS][P2CordNUM_COLUMNS];
     boolean CordBoard = false;
     
     public static void Reset() {
@@ -28,8 +34,14 @@ public class Board {
    
     }
     
+     public Player getcurrentPlayer() {
+         Player currentTurn = new currentTurn();
+         return(currentTurn);
+     }
+    
     public static void Draw(Graphics2D g) {
      //draw grid
+     if (currentTurn == players[1]) {
        if (Cordboard == false) {
         int ydelta = Window.getHeight2()/NUM_ROWS;
         int xdelta = Window.getWidth2()/NUM_COLUMNS;
@@ -82,6 +94,62 @@ public class Board {
             }
         }   
        }
+     }
+     
+     if (currentTurn == players[2]) {
+       if (Cordboard == false) {
+        int ydelta = Window.getHeight2()/P2NUM_ROWS;
+        int xdelta = Window.getWidth2()/P2NUM_COLUMNS;
+    //    System.out.println(xdelta + " " + ydelta);
+ 
+        g.setColor(Color.black);
+        for (int zi = 1;zi<P2NUM_ROWS;zi++)
+        {
+            g.drawLine(Window.getX(0),Window.getY(zi*ydelta),
+                    Window.getX(Window.getWidth2()),Window.getY(zi*ydelta));
+        }
+        
+        for (int zi = 1;zi<P2NUM_COLUMNS;zi++)
+        {
+            g.drawLine(Window.getX(zi*xdelta),Window.getY(0),
+                    Window.getX(zi*xdelta),Window.getY(Window.getHeight2()));
+        }
+        for (int zrow=0;zrow<P2NUM_ROWS;zrow++)
+        {
+            for (int zcol=0;zcol<P2NUM_COLUMNS;zcol++)        
+            {
+                if (board[zrow][zcol] != null)
+                    board[zrow][zcol].draw(g, zrow, zcol,xdelta, ydelta);
+            }
+        }   
+       }
+        if (Cordboard == true) {
+        int ydelta = Window.getHeight2()/P2CordNUM_ROWS;
+        int xdelta = Window.getWidth2()/P2CordNUM_COLUMNS;
+    //    System.out.println(xdelta + " " + ydelta);
+ 
+        g.setColor(Color.black);
+        for (int zi = 1;zi<P2CordNUM_ROWS;zi++)
+        {
+            g.drawLine(Window.getX(0),Window.getY(zi*ydelta),
+                    Window.getX(Window.getWidth2()),Window.getY(zi*ydelta));
+        }
+        
+        for (int zi = 1;zi<P2CordNUM_COLUMNS;zi++)
+        {
+            g.drawLine(Window.getX(zi*xdelta),Window.getY(0),
+                    Window.getX(zi*xdelta),Window.getY(Window.getHeight2()));
+        }
+        for (int zrow=0;zrow<P2CordNUM_ROWS;zrow++)
+        {
+            for (int zcol=0;zcol<P2CordNUM_COLUMNS;zcol++)        
+            {
+                if (Mboard[zrow][zcol] != null)
+                    Mboard[zrow][zcol].draw(g, zrow, zcol,xdelta, ydelta);
+            }
+        }   
+       }
+     }
       /*  if (winner == WinState.WIN_P1) {
             g.setColor(Color.red);
             g.setFont(new Font("Arial",Font.PLAIN,50));
